c0f6ded7a3d324e04ef9d5dd0da06a3bff25afa3
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-13 18:11:52 +0600

    feat: add  a beautiful register screen to register the user to the mysql database

---
+++ src/screens/register-screen.jsx
@@ -0,0 +1,100 @@
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/form-container";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const RegisterScreen = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      return;
+    }
+
+    try {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      const res = await axios.post("http://localhost:8000/api/users", userData);
+
+      toast.success("Registered successfully");
+      localStorage.setItem("userInfo", JSON.stringify(res.data.data));
+      navigate(location?.state?.from ? location?.state?.from : "/");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h1>Register</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="my-2" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className="my-2" controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="mt-3">
+          {isLoading ? "Signing up..." : "Register"}
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          Already have an account? <Link to={`/login`}>Login</Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
