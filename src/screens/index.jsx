7bb3b9ad108b726a7baf1c8f8a24767311803a77
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-13 18:13:58 +0600

    chore: add index js for one line export

---
+++ src/screens/index.jsx
@@ -0,0 +1,13 @@
+import HomeScreen from "./home-screen";
+import LoginScreen from "./login-screen";
+import RegisterScreen from "./register-screen";
+import TimeEntryScreen from "./time-entry-screen";
+import WeeklyTimeViewScreen from "./weekly-time-view-screen";
+
+export {
+  HomeScreen,
+  LoginScreen,
+  RegisterScreen,
+  TimeEntryScreen,
+  WeeklyTimeViewScreen,
+};
